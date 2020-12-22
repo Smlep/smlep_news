fe13d33b0793197cefe63e3b22dd12cbad7c2036
Sam Courtemanche <sam.courtemanche@epita.fr>
2020-12-22 23:22:45 +0100

    medium: init app and add parsing from specific topic

---
+++ smlep_news/medium/__init__.py
@@ -0,0 +1,32 @@
+import feedparser
+
+from datetime import datetime
+from time import mktime
+
+
+class Story:
+    def __init__(self, d: feedparser.util.FeedParserDict):
+        self.title = d.title
+        self.html_summary = d.summary
+        self.author = d.author
+        self.link = d.link
+        self.id_link = d.id
+
+    def __repr__(self):
+        return "{} from {}".format(self.title, self.author)
+
+
+class Feed:
+    def __init__(self, topic: str, d: feedparser.util.FeedParserDict):
+        self.topic = topic
+        self.updated = datetime.fromtimestamp(mktime(d.feed["updated_parsed"]))
+        self.stories = [Story(e) for e in d.entries]
+
+    def __repr__(self):
+        return "{} feed on {}".format(self.topic, self.updated)
+
+
+def get_top_topic_stories(topic: str):
+    stories = feedparser.parse("https://medium.com/feed/topic/{}".format(topic))
+
+    return Feed(topic, stories)
