95f192e11deb7ba0d237d5ef5fb47e19b7ff9882
Sam Courtemanche <sam.courtemanche@epita.fr>
2020-12-24 00:01:54 +0100

    medium: add function to build topics list and current topics

--- smlep_news/medium/__init__.py
+++ smlep_news/medium/__init__.py
@@ -1,8 +1,13 @@
 import feedparser
+import requests
 import time
 
+from bs4 import BeautifulSoup
 from datetime import datetime
 from time import mktime
+
+topic_feed_url = "https://medium.com/feed/topic/{}"
+topics_page_url = "https://medium.com/topics"
 
 
 class Story:
@@ -33,10 +38,28 @@ def get_top_topic_stories(topic: str, retry_count=1, retry_interval=0):
     To fix this, increase the retry_count and retry_interval.
     """
     for i in range(retry_count):
-        stories = feedparser.parse("https://medium.com/feed/topic/{}".format(topic))
+        stories = feedparser.parse(topic_feed_url.format(topic))
 
         if not stories.bozo:
             break
         time.sleep(retry_interval)
 
     return Feed(topic, stories)
+
+
+def build_topics_list():
+    """
+    Builds topics list.
+    This function relies on scrapping the topics page, meaning it is not robust
+    and might stop working.
+
+    To avoid calling it every time, a list of topics is stores in medium/topics.json
+    """
+    response = requests.get(topics_page_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    topics_links = soup.select("a[href*=\/topic\/]")
+    return {
+        t.text: {"link": t.get("href"), "short_name": t.get("href").split("/")[-1]}
+        for t in topics_links
+        if t.text
+    }
