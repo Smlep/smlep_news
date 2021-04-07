25f5f2a1af7870381c2a8ffb3995e741cdeb79c0
Sam Courtemanche <sam.courtemanche@epita.fr>
2021-04-07 21:15:30 +0200

    exchangerates: update to match new version of api

--- smlep_news/exchangerates/__init__.py
+++ smlep_news/exchangerates/__init__.py
@@ -2,11 +2,12 @@ import requests
 
 from .rates import Rates
 
-url = "https://api.exchangeratesapi.io/latest"
+url = "http://api.exchangeratesapi.io/v1/latest"
 
 
-def get_exchange_rates():
-    r = requests.get(url)
+def get_exchange_rates(key):
+    full_url = f"{url}?access_key={key}"
+    r = requests.get(full_url)
     r.raise_for_status()
 
     return Rates(r.json())
