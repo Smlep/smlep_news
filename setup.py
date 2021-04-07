c4de1cabace00d5839400af33f8373b3298bc04b
Sam Courtemanche <sam.courtemanche@epita.fr>
2021-04-07 21:16:21 +0200

    misc: bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smlep_news",
-    version="0.3.0",
+    version="0.3.1",
     description="Custom news scrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
